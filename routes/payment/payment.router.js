394bb032ed3ca98705f042cc8f18aeee1ee00d73
tuan123viname <vungoctuan9a6dt@gmail.com>
2020-06-18 16:34:52 +0700

    update api pay

--- routes/payment/payment.router.js
+++ routes/payment/payment.router.js
@@ -1,5 +1,6 @@
 const Router = require("express").Router();
 const verifyToken = require("../../middleware/verifyToken");
+const env = require('dotenv').config();
 const { PRIVATE_KEY_PAYMENT } = process.env;
 const stripe = require('stripe')(PRIVATE_KEY_PAYMENT); 
 const courseController = require("../../controller/course.controller");
@@ -84,15 +85,63 @@ Router.post("/pay", async (req, res) => {
         //     })
         //     .catch(err => console.log(err));
 
-        stripe.charges.create({
-            amount:9000,
-            source:req.body.stripeTokenId,
-            currency:'usd'
-        }).then(()=>{
-            console.log('paySuccess');
-        }).catch(()=>{
-            console.log('pay false');
-        })
+        // stripe.charges.create({
+        //     amount:9000,
+        //     source:req.body.stripeTokenId,
+        //     currency:'usd'
+        // }).then(()=>{
+        //     console.log('paySuccess');
+        // }).catch(()=>{
+        //     console.log('pay false');
+        // })
+        var totalAmount=0;
+        console.log(req.body);
+        for(let i=0 ; i<req.body.cart.length;i++){
+            var courseDetail = await courseController.getbyId(req.body.cart[i]._id);
+            var mustPay = courseDetail.price-((courseDetail.price*courseDetail.discount)/100);
+            mustPay = Math.round(mustPay);
+            totalAmount=totalAmount+mustPay;
+        }
+
+        const { token, cart, idUser }  = req.body;
+        const customer = await stripe.customers.create({
+            email : token.email,
+            source : token.id
+        });
+
+        const charges = await stripe.charges.create({
+            amount : totalAmount,
+            currency : 'vnd',
+            customer : customer.id,
+            receipt_email : token.email,
+            description : 'mua khoa hoc',
+            shipping : {
+                name : token.card.name,
+                address : {
+                    line1 : token.card.address_line1,
+                    city : token.card.address_city,
+                    country : token.card.address_country,
+                    postal_code : token.card.address_zip
+                }
+            }, 
+        });
+        console.log(charges);
+       
+
+        for(let i=0;i<req.body.cart.length;i++){
+            await orderController.createOrder({
+                "idUser":req.body.idUser,
+                "idCourse":req.body.cart[i]._id,
+                "amount": totalAmount
+            });
+
+            await joinController.joinCourse({
+                "idUser":req.body.idUser,
+                "idCourse":req.body.cart[i]._id
+            });
+        }
+        res.status(200).send({ success : true, message : 'mua khóa học thành công!'});
+
     } catch (err) {
         console.log('err',err);
       res.status(500).send(err);
